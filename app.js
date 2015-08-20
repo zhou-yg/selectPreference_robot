e4a64320c596375928d63a078b36937599596e8b
zhou-yg <zhou.y.g890@gmail.com>
2015-08-21 00:45:12 +0800

    先爬个小的

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 var Promise = require('promise');
+var Q = require('q');
+var fs = require('fs');
 
+var allStars = require('./lib/allStars');
+
+var allStarsMain = allStars();
+
+var allStarsArr = [];
+
+var oneNum = 20;
+
+var saveFile = './data/allStars.json';
+
+for(var i=0;i<2;i++){
+    allStarsMain({
+        startIndex:oneNum*i,
+        everyNum:oneNum
+    }).done(function(allStars){
+        try{
+            allStarsArr = allStarsArr.concat(allStars);
+            console.log('allStars:out:',allStars.length);
+
+            fs.write(saveFile,JSON.stringify(allStarsArr,null,2),'w');
+
+        }catch (e){
+            console.log(e);
+        }
+    });
+}
