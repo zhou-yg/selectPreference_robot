529331e251269682f5bfcaf82847f439911d933e
zhou-yg <zhou.y.g890@gmail.com>
2015-08-21 23:26:05 +0800

    readdy query baike

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ var Promise = require('promise');
 var Q = require('q');
 var fs = require('fs');
 
-var allStars = require('./lib/allStars');
+var allStars = require('./lib/allStars'),
+    queryMessageStarsBaike = require('./lib/allStarsBaike')
 
 var allStarsMain = allStars();
 
-var allStarsArr = [];
+var oneNum = 100;
 
-var oneNum = 20;
+var saveFile = './data/allStars.json',
+    saveBaikeFile = './data/allBaikeStars.json';
 
-var saveFile = './data/allStars.json';
+//爬百科
+var queryBaike = function (allStarsArr) {
+    var d = Q.defer();
 
-for(var i=0;i<2;i++){
+    queryMessageStarsBaike(allStarsArr,function(allStarsBaikeArr){
+        d.resolve(allStarsBaikeArr)
+    });
+
+    return d.promise;
+};
+
+//递归的爬
+var queryFn = function(allStarsArr,unitNum,number,whenEnd){
+    if(!number){
+        number = 0;
+    }
+
     allStarsMain({
-        startIndex:oneNum*i,
-        everyNum:oneNum
+        startIndex:unitNum*number,
+        everyNum:unitNum
     }).done(function(allStars){
         try{
             allStarsArr = allStarsArr.concat(allStars);
@@ -23,8 +39,19 @@ for(var i=0;i<2;i++){
 
             fs.write(saveFile,JSON.stringify(allStarsArr,null,2),'w');
 
+            if(allStars.length>0){
+                queryFn(allStarsArr,unitNum,number+1,whenEnd);
+            }else{
+                whenEnd(allStarsArr);
+            }
         }catch (e){
             console.log(e);
         }
     });
-}
+};
+
+queryFn([],oneNum,0,function(allStarsArr){
+    queryBaike(allStarsArr).done(function(allStarsArr){
+        fs.write(saveBaikeFile,JSON.stringify(allStarsArr,null,2),'w');
+    });
+});
